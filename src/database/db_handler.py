e90645e1b3a78544f2d6524c76425e3342e85ade
Antti Aarnio <laborantti.antti@gmail.com>
2022-11-17 17:12:44 +0200

    vk4: lisätty pylint, korjattu ilmoitetut virheet tyylissä

--- src/database/db_handler.py
+++ src/database/db_handler.py
@@ -6,142 +6,236 @@ import re
 import os
 
 # class implementation for Database_handler, class that will handle all interaction with sql-db
-class Database_handler:
-  # protected private variable for class that predefines strings for table creation step of a new database
-  __table_creation = [
-    "CREATE TABLE Products (id INTEGER PRIMARY KEY, name TEXT NOT NULL UNIQUE, type INTEGER NOT NULL, subtype DEFAULT 0, storage_life INTEGER NOT NULL)",
-    "CREATE TABLE Types (id INTEGER PRIMARY KEY, type_name TEXT NOT NULL UNIQUE)",
-    "CREATE TABLE Subtypes (id INTEGER PRIMARY KEY, subtype_name TEXT NOT NULL UNIQUE, type INTEGER NOT NULL)"
-  ]
+class DatabaseHandler:
+  # protected private variable for class that predefines
+  # strings for table creation step of a new database
+    __table_creation = [
+        """CREATE TABLE Products (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL UNIQUE,
+            type INTEGER NOT NULL,
+            subtype INTEGER NOT NULL,
+            storage_life INTEGER NOT NULL,
+            number_of INTEGER NOT NULL
+        )""",
+        """CREATE TABLE Types (
+            id INTEGER PRIMARY KEY,
+            type_name TEXT NOT NULL UNIQUE
+        )""",
+        """CREATE TABLE Subtypes (
+            id INTEGER PRIMARY KEY,
+            subtype_name TEXT NOT NULL UNIQUE,
+            type INTEGER NOT NULL
+        )"""
+    ]
 
-  __subtype_creation = [
-    "Hedelmät",
-    "Juomajauheet",
-    "Juomatiivisteet",
-    "Kalat",
-    "Kasvikset",
-    "Leivonta",
-    "Lihat",
-    "Maitotuotteet",
-    "Marjat",
-    "Mausteet",
-    "Maustekastikkeet",
-    "Munat",
-    "Puolivalmisteet",
-    "Rasvat, öljyt",
-    "Ravintojauheet",
-    "Sokerit",
-    "Säilykkeet",
-    "Vihannekset",
-    "Viljatuotteet"
-  ]
+    __subtype_creation = [
+        "Hedelmät",
+        "Juomajauheet",
+        "Juomatiivisteet",
+        "Kalat",
+        "Kasvikset",
+        "Leivonta",
+        "Lihat",
+        "Maitotuotteet",
+        "Marjat",
+        "Mausteet",
+        "Maustekastikkeet",
+        "Munat",
+        "Puolivalmisteet",
+        "Rasvat, öljyt",
+        "Ravintojauheet",
+        "Sokerit",
+        "Säilykkeet",
+        "Vihannekset",
+        "Viljatuotteet"
+    ]
 
-  __type_creation = [
-    "Juomat",
-    "Ruoat",
-    "Raaka-aineet"
-  ]
+    __type_creation = [
+        "Juomat",
+        "Ruoat",
+        "Raaka-aineet"
+    ]
 
 
-  # protected private variable for checking the database path
-  __db_path_re = ["src/database/", ".db"]
+    # protected private variable for checking the database path
+    __db_path_re = ["src/database/", ".db"]
 
-  # protected private function to check validity of database path string
-  # minimum length 17 characters (path + '.db' + db file name at least 1 character)
-  # regular expressions: path contains "src/database/" path and file type extension '.db'
-  def __check_db_path(self, str):
-    str_length = True if len(str) >= 17 else False
-    str_pt1_validity = True if re.search(self.__db_path_re[0],str) != None else False
-    str_pt2_validity = True if re.search(self.__db_path_re[1],str) != None else False
-    
-    return True if str_length and str_pt1_validity and str_pt2_validity else False
+    # protected private function to check validity of database path string
+    # minimum length 17 characters (path + '.db' + db file name at least 1 character)
+    # regular expressions: path contains "src/database/" path and file type extension '.db'
+    def __check_db_path(self, db_str:str):
+        str_length = bool(len(db_str) >= 17)
+        str_pt1_validity = bool(re.search(self.__db_path_re[0],db_str))
+        str_pt2_validity = bool(re.search(self.__db_path_re[1],db_str))
 
-  # class initialization
-  # > if db_set is True
-  # > database contains valid database path
-  # > path leads to valid file
-  # >> connect to database file
-  # > else create new database with automatic table creation
-  def __init__(self,db_set,database):
-    if db_set and database != None and self.__check_db_path(database) and os.path.isfilget_subtypecounte(database):
-      self._db = sqlite3.connect(database)
-    else:
-      self._db = sqlite3.connect("src/database/pantry.db")
-      for item in self.__table_creation:
+        return bool(str_length and str_pt1_validity and str_pt2_validity)
+
+    def __check_validity(self,db_path):
+        if db_path is not None:
+            return self.__check_db_path(db_path)
+        return False
+
+    def __fetch_typeid(self, typename:str):
+        return self._db.execute("SELECT id FROM Types WHERE type_name=?",[typename]).fetchone()
+
+    # class initialization
+    # > if db_set is True
+    # > database contains valid database path
+    # > path leads to valid file
+    # >> connect to database file
+    # > else create new database with automatic table creation
+    def __init__(self,db_set:bool,database:str):  # pylint: disable=too-many-statements
+        if db_set and self.__check_validity(database) and os.path.isfile(database):
+            self._db = sqlite3.connect(database)
+        elif not db_set and database is not None and self.__check_db_path(database):
+            self._db = sqlite3.connect(database)
+            for item in self.__table_creation:
+                try:
+                    self._db.execute(item)
+                except: # pylint: disable=bare-except
+                    print("table already exists")
+            for item in self.__type_creation:
+                self.add_type(item)
+            __type_id = self.__fetch_typeid("Raaka-aineet")[0]
+            for item in self.__subtype_creation:
+                self.add_subtype(item, __type_id)
+        else:
+            self._db = sqlite3.connect("src/database/pantry.db")
+            for item in self.__table_creation:
+                try:
+                    self._db.execute(item)
+                except: # pylint: disable=bare-except
+                    print("table already exists")
+            for item in self.__type_creation:
+                self.add_type(item)
+            __type_id = self.__fetch_typeid("Raaka-aineet")[0]
+            for item in self.__subtype_creation:
+                self.add_subtype(item, __type_id)
+        self._db.isolation_level=None
+
+    # function for adding new product with its name,
+    # type, subtype and storage_life as arguments
+    # name (string), type (int) and storage_life (int) needed,
+    # subtype (int) optional, count (int) optional
+    def add_product(self, name:str,type_of:int,storage_life:int,subtype:int=0,count:int=1):
         try:
-          self._db.execute(item)
-        except:
-          print("table already exists")
-      
-      for item in self.__type_creation:
-        self.add_type(item)
-      
-      __type_id = self._db.execute("SELECT id FROM Types WHERE type_name=?",["Raaka-aineet"]).fetchone()[0]
-      
-      for item in self.__subtype_creation:
-        self.add_subtype(item, __type_id)
+            self._db.execute("""
+                INSERT INTO Products (
+                    name,
+                    type,
+                    subtype,
+                    storage_life,
+                    number_of
+                ) VALUES (?,?,?,?,?)""", [name, type_of, subtype, storage_life, count])
+            return True
+        except: # pylint: disable=bare-except
+            return False
 
-    self._db.isolation_level=None
-  
-  # function for adding new product with its name, type, subtype and storage_life as arguments
-  # name (string), type (int) and storage_life (int) needed, subtype (int) optional
-  def add_product(self, name:str,type:int,storage_life:int,subtype:int=0):
-    try:
-      self._db.execute("INSERT INTO Products (name, type, subtype, storage_life) VALUES (?,?,?,?)", [name, type, subtype, storage_life])
-      return True
-    except:
-      return False
+    # function for fetching products from db
+    # returns None if no product in db
+    # joins Product with Type and conditionally joins with Subtype
+    # if Product.subtype has value greater than 0
+    def get_products(self):
+        products = self._db.execute("""
+            SELECT
+                R.*,
+                S.subtype_name AS subtype_name FROM (
+                    SELECT
+                        P.id AS pid,
+                        P.name AS pname,
+                        P.storage_life AS storage_life,
+                        P.number_of AS number_of,
+                        T.type_name AS type_name,
+                        P.subtype AS sid FROM
+                            Products P LEFT JOIN Types T
+                                ON P.type=T.id) R LEFT JOIN Subtypes S
+                                    ON R.sid AND R.sid=S.id
+        """).fetchall()
+        return products if len(products) > 0 else None
 
-  # function for fetching products from db
-  # returns None if no product in db
-  # joins Product with Type and conditionally joins with Subtype if Product.subtype has value greater than 0
-  def get_products(self):
-    products = self._db.execute("SELECT R.*, S.subtype_name AS subtype_name FROM (SELECT P.id AS pid, P.name AS pname, P.storage_life AS storage_life, T.type_name AS type_name, P.subtype AS sid FROM Products P LEFT JOIN Types T ON P.type=T.id) R LEFT JOIN Subtypes S ON R.sid AND R.sid=S.id").fetchall()
-    return products if len(products) > 0 else None
+    # function for fetching number of products in db
+    def get_productcount(self):
+        count = self._db.execute("SELECT count(*) FROM Products").fetchall()[0][0]
+        return count
 
-  # function for adding new type with its name as argument
-  # optional argument subtype (integer which is the id of subtype)
-  def add_type(self,name:str):
-    try:
-      self._db.execute("INSERT INTO Types (type_name) VALUES (?)", [name])
-      return True
-    except:
-      return False
+    # function for removing product from db
+    def remove_product(self,product_id:int):
+        try:
+            self._db.execute("DELETE FROM Products WHERE id=?",[product_id])
+            return True
+        except: # pylint: disable=bare-except
+            return False
 
-  # function for fetching types from db
-  # returns None if no types in db
-  def get_types(self):
-    types = self._db.execute("SELECT type_name FROM Types").fetchall()
-    return types if len(types) > 0 else None
-  
-  # function for fetching number of types in db
-  def get_typecount(self):
-    count = self._db.execute("SELECT count(*) FROM Types").fetchall()[0][0]
-    return count
+    # function for updating number of products with certain id
+    def update_count(self,product_id:int,change:int=1,subtract:bool=True):
+        _product = self._db.execute("""
+            SELECT id, number_of FROM Products WHERE id=?
+        """,[product_id]).fetchone()
+        if subtract and _product[1]-change == 0:
+            return self.remove_product(id)
+        if subtract:
+            try:
+                self._db.execute("""
+                    UPDATE Products SET number_of=number_of-? WHERE id=?
+                """,[change,product_id])
+                return True
+            except: # pylint: disable=bare-except
+                return False
+        else:
+            try:
+                self._db.execute("""
+                    UPDATE Products SET number_of=number_of+? WHERE id=?
+                """,[change,product_id])
+                return True
+            except: # pylint: disable=bare-except
+                return False
 
-  # function for adding new subtype with its name as argument
-  # adding fails, if exact same name already exists in db
-  def add_subtype(self,name:str, type:int):
-    try:
-      self._db.execute("INSERT INTO Subtypes (subtype_name, type) VALUES (?,?)",[name,type])
-      return True
-    except:
-      return False
-  
-  # function for fetching subtypes from db
-  # returns None if no subtypes in db
-  def get_subtypes(self):
-    subtypes = self._db.execute("SELECT type_name, subtype_name FROM Subtypes S LEFT JOIN Types T ON S.type=T.id").fetchall()
-    return subtypes if len(subtypes) > 0 else None
+    # function for adding new type with its name as argument
+    # optional argument subtype (integer which is the id of subtype)
+    def add_type(self,name:str):
+        try:
+            self._db.execute("INSERT INTO Types (type_name) VALUES (?)", [name])
+            return True
+        except: # pylint: disable=bare-except
+            return False
 
-  # function for fetching number of subtypes in db
-  def get_subtypecount(self):
-    count = self._db.execute("SELECT count(*) FROM Subtypes").fetchall()[0][0]
-    return count
+    # function for fetching types from db
+    # returns None if no types in db
+    def get_types(self):
+        types = self._db.execute("SELECT type_name FROM Types").fetchall()
+        return types if len(types) > 0 else None
 
-  # function for deleting a subtype from db with its name as argument
-  # TODO
+    # function for fetching number of types in db
+    def get_typecount(self):
+        count = self._db.execute("SELECT count(*) FROM Types").fetchall()[0][0]
+        return count
 
-  # function for returning db status (???)
-  def __str__(self):
-    return "Pantry tietokanta"
+    # function for adding new subtype with its name as argument
+    # adding fails, if exact same name already exists in db
+    def add_subtype(self,name:str, type_of:int):
+        try:
+            self._db.execute("""
+                INSERT INTO Subtypes (subtype_name, type) VALUES (?,?)
+            """,[name,type_of])
+            return True
+        except: # pylint: disable=bare-except
+            return False
+
+    # function for fetching subtypes from db
+    # returns None if no subtypes in db
+    def get_subtypes(self):
+        subtypes = self._db.execute("""
+            SELECT type_name, subtype_name FROM Subtypes S LEFT JOIN Types T ON S.type=T.id
+        """).fetchall()
+        return subtypes if len(subtypes) > 0 else None
+
+    # function for fetching number of subtypes in db
+    def get_subtypecount(self):
+        count = self._db.execute("SELECT count(*) FROM Subtypes").fetchall()[0][0]
+        return count
+
+    # function for deleting a subtype from db
+    # with its name as argument
+
+    # function for returning db status (???)
--- src/pantry.py
+++ src/pantry.py
@@ -1,34 +1,33 @@
 # depends on tkinter for UI creation, import tkinter library for python3
 # additionally imports db_handler from projects database-directory
 
-from tkinter import Tk, ttk
+from tkinter import Tk
 
-from database.db_handler import Database_handler
+from database.db_handler import DatabaseHandler
 from ui.add_product import AddProduct
 
 # class implementation for the base of Pantry UI
 
-class Pantry_UI:
+class PantryUI:
+    # class initialization
+    # connect or create database
+    def __init__(self, root, db_path):
+        self._root = root
+        self._current_view = None
+        self._dbh = DatabaseHandler(True, db_path)
 
-  # class initialization
-  # connect or create database
-  def __init__(self, root, db_path):
-    self._root = root
-    self._current_view = None
+    # start Pantry application
+    def start(self):
+        self._show_add_product()
 
-  # start Pantry application
-  def start(self, label_text):
-    self._dbh = Database_handler(True, "src/database/pantry_db.db")
-    self._show_add_product()
-  
-  def _show_add_product(self):
-    self._current_view = AddProduct(self._root, self._dbh)
-    self._current_view.pack()
+    def _show_add_product(self):
+        self._current_view = AddProduct(self._root, self._dbh)
+        self._current_view.pack()
 
 window = Tk()
 window.title("Pantry")
 
-ui = Pantry_UI(window,"src/database/pantry.db")
-ui.start("Pantry")
+ui = PantryUI(window,"src/database/pantry.db")
+ui.start()
 
 window.mainloop()
